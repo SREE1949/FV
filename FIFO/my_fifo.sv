3d455e2c1f57046282968ebc6b72853e80640cfc
SREEKANTH <98770751+SREE1949@users.noreply.github.com>
2023-12-02 16:07:03 +0530

    Update my_fifo.sv

--- FIFO/my_fifo.sv
+++ FIFO/my_fifo.sv
@@ -39,3 +39,49 @@ module fifo
   assign empty = size==0 ? 1'b1 : 1'b0;
   
 endmodule
+
+
+
+
+`ifdef FORMAL
+
+       //	as_in1: assume property(!we && re);
+	//as_in2: assume property(we && !re);
+	
+	always@(posedge clk) begin
+		if(~rst) begin
+			a_size: assert (size == 0
+                           || size == $past(size)
+                           || size == $past(size) + 1
+                           || size == $past(size) - 1); //always increment or decrement by one
+
+			
+		//	a_in_con1: assert(we && re); 
+		 //      	a_in_con2: assert(!we && re);
+                        as_in1: assume property(!we && re);
+                        as_in2: assume property(we && !re);
+
+
+			c_full: cover (full && size==FIFO_DEPTH-1); // fifo full condition
+			c_empty: cover (empty && size==0); //fifo empty condition
+			
+		   end
+	 end
+       
+
+
+`ifdef VERIFIC
+
+                	ap_rptr: assert property (@(posedge clk) disable iff (rst) re |=> $changed(rptr));
+                        ap_wptr: assert property (@(posedge clk) disable iff (rst) we |=> $changed(wptr));  
+	
+                        ap_rptr: assert property (@(posedge clk) disable iff (rst) !re && !full  |=> $stable(rptr));
+                        ap_wptr: assert property (@(posedge clk) disable iff (rst) !we && !empty |=> $stable(wptr));
+
+
+  
+
+`endif
+
+`endif 
+endmodule
